36e24b7318011fedb57109398173f02b8f8b887a
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-07-08 13:45:22 -0300

    Hice que las cards se vean en el medio en celulares

--- components/ListaPeliculas.jsx
+++ components/ListaPeliculas.jsx
@@ -3,9 +3,9 @@ import CardPelicula from "./CardPelicula";
 
 const ListaPeliculas = ({ peliculas }) => {
     return (
-        <section className="my-3 lista-peliculas">
+        <section className="my-3 lista-peliculas d-flex flex-column">
             <h2 className="my-3 text-light text-center">Lista de Películas</h2>
-            <div className="w-100 row">
+            <div className="w-100 row mx-auto">
                 {
                     peliculas.map((pelicula , indice) => (
                         <div key = {indice} className="col-md-4 mb-3">
