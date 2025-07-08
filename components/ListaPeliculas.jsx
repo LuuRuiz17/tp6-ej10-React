db22d4511b2f9caa397f20c914ea8e0d281074ca
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-07-08 13:27:09 -0300

    Al hacer el deploy la lista de películas perdió el formato que le di

--- components/ListaPeliculas.jsx
+++ components/ListaPeliculas.jsx
@@ -5,7 +5,7 @@ const ListaPeliculas = ({ peliculas }) => {
     return (
         <section className="my-3 lista-peliculas">
             <h2 className="my-3 text-light text-center">Lista de Películas</h2>
-            <Row>
+            <Row className="w-100">
                 {
                     peliculas.map((pelicula , indice) => (
                         <div key = {indice} className="col-md-4 mb-3">
